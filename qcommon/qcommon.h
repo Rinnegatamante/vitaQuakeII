b4347faac8b8abf5a4f1421c3dbd7df4cbccd208
Eduardo Mínguez <edu@redhat.com>
2020-11-30 12:10:43 +0100

    Updated version (#41)

--- qcommon/qcommon.h
+++ qcommon/qcommon.h
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "../game/q_shared.h"
 
 
-#define	VERSION		2.3
+#define	VERSION		2.4
 
 #define	BASEDIRNAME	"baseq2"
 
